7ef367c1cc801ff363ed4e0d34c69301b917b6a6
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-06-03 15:42:48 +0530

    ADDED:To check maximum number

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -1,7 +1,21 @@
 package com.bridgelabz;
 
 public class Maximum {
+
+    public void findMaximumInteger()
+    {
+        int[] array = new int[]{10,20,30};
+        int currentMax = array[0];
+        for(int index=1;index<=array.length-1;index++){
+            if(currentMax < array[index]){
+                currentMax = array[index];
+            }
+        }
+        System.out.println("Maximum value is "+ currentMax);
+    }
     public static void main(String[] args) {
-        System.out.println("Wel come generic using test maximum problem");
+        Maximum maximum = new Maximum();
+        maximum.findMaximumInteger();
     }
 }
+
