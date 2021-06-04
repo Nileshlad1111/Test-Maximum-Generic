d8e9940a5e1933f80659fd27f8d3e61280df3531
Nileshlad1111 <nileshcareer1111@gmail.com>
2021-06-04 18:07:12 +0530

    ADDED:To print maximum value

--- src/main/java/com/bridgelabz/Maximum.java
+++ src/main/java/com/bridgelabz/Maximum.java
@@ -16,11 +16,17 @@ public class Maximum <E extends Comparable> {
         this.thirdValue = thirdValue;
     }
 
-    public E findMaximum() {
-        return findMaximum(firstValue, secondValue, thirdValue);
+    public E findMaximum(){
+        E maximum = findMaximum(firstValue, secondValue, thirdValue);
+        printMax(maximum);
+        return maximum;
     }
 
-    public static < E extends Comparable> E findMaximum(E firstValue , E secondValue , E thirdValue) {
+    public <E> void printMax(E maximum){
+        System.out.println(maximum);
+    }
+
+    public static < E extends Comparable> E findMaximum (E firstValue , E secondValue , E thirdValue) {
         E maximumValue = firstValue;
         if (secondValue.compareTo(maximumValue) > 0)
             maximumValue = secondValue;
@@ -29,7 +35,7 @@ public class Maximum <E extends Comparable> {
         return maximumValue;
     }
 
-    public static void main(String[] args) {
+    public static void main(String[] args){
         System.out.println("Wel come generic using test maximum problem");
     }
 }
